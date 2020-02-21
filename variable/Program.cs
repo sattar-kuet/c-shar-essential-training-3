966b30ba384424a57b840443a49fb4a3cca3d162
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:37:46 +0600

    first commit

---
+++ variable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            number = number + 10;
+            number = 10;
+            number -= 2;
+
+            Console.WriteLine(number);
+
+        }
+    }
+}
